1dd699a434a3ebfb1ff24f82baf92617bef9c09a
Sebastien Arbogast <sebastien.arbogast@gmail.com>
2018-01-18 21:44:53 +0100

    adding frontend files to Github pages

--- truffle.js
+++ truffle.js
@@ -18,6 +18,14 @@ module.exports = {
             port: 8545,
             network_id: 4, //rinkeby test network
             gas: 4700000
+          },
+          live: {
+            host: "localhost",
+            port: 8545,
+            network_id: 1,
+            gas: 4700000,
+            gasPrice: 57000000000,
+            from: "0xbf6f5e78e154799c7856f4cf9f269cf7a7201310"
           }
      }
 };
